7c89f4e7f7fb80dcfc7b2f78cf2f61c82e34e5ab
Chris Strasser <Strasser@BRK-ITLT11-001.local>
2015-08-31 19:20:18 -0400

    added pagination to home

--- organization/views.py
+++ organization/views.py
@@ -5,7 +5,5 @@ from main.models import Organization, Location,Contact
 
 def orgview(request,org_id):
     organization = Organization.objects.get(pk =org_id)
-    
-    
-    
+         
     return render(request,'orgform.html',{'orginfo':organization})
